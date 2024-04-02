a4101ff77c732216ac835f3a52a4216349c1344c
jhpark1227 <junhyeok0246@naver.com>
2024-04-02 18:18:19 +0900

    initial commit

---
+++ src/main/java/junhyeok/giveme/GivemeApplication.java
@@ -0,0 +1,13 @@
+package junhyeok.giveme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GivemeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/junhyeok/giveme/GivemeApplicationTests.java
@@ -0,0 +1,13 @@
+package junhyeok.giveme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GivemeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
