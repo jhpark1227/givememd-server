24f36f4096188aabdbc0ccdfe90dc41583baa71e
jhpark1227 <junhyeok0246@naver.com>
2024-04-02 20:34:47 +0900

    feat : JWT 인증/인가를 위한 Security filterChain 기본 설정

---
+++ src/main/java/junhyeok/giveme/global/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package junhyeok.giveme.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .formLogin(FormLoginConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .logout(LogoutConfigurer::disable)
+                .sessionManagement(config->config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        config->
+                                config.anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
