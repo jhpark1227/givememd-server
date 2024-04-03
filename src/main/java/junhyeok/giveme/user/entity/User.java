3b99f67e3d94f1d2baa1d3ab3a02fd9042e0a826
jhpark1227 <junhyeok0246@naver.com>
2024-04-03 19:55:20 +0900

    feat : User 엔티티 구현

---
+++ src/main/java/junhyeok/giveme/user/entity/User.java
@@ -0,0 +1,37 @@
+package junhyeok.giveme.user.entity;
+
+import jakarta.persistence.*;
+import junhyeok.giveme.user.dto.response.GithubProfile;
+import junhyeok.giveme.user.enums.Role;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder @NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
+public class User {
+    @Id
+    private Long id;
+
+    private String githubId;
+
+    private String name;
+
+    private String image;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(15) DEFAULT 'USER'")
+    private Role role;
+
+    public User(GithubProfile profile){
+        this.id = profile.getId();
+        this.githubId = profile.getGithubId();
+        this.name = profile.getName();
+        this.image = profile.getImage();
+    }
+
+    public void changeProfile(GithubProfile profile){
+        this.githubId = profile.getGithubId();
+        this.name = profile.getName();
+        this.image = profile.getImage();
+    }
+}
