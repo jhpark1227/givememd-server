0f58b9aa69a8e247cd20eefa888815f57cb01169
jhpark1227 <junhyeok0246@naver.com>
2024-04-07 00:03:12 +0900

    feat : Readme Entity 구현

---
+++ src/main/java/junhyeok/giveme/readme/entity/Readme.java
@@ -0,0 +1,20 @@
+package junhyeok.giveme.readme.entity;
+
+import jakarta.persistence.*;
+import junhyeok.giveme.user.entity.User;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder @NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
+public class Readme {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private String name;
+
+    @ManyToOne @JoinColumn(name = "user_id")
+    private User user;
+}
