a9215ca57ce2dfe800dab3e3f6ee3c4255a05ed9
jhpark1227 <junhyeok0246@naver.com>
2024-04-08 20:25:07 +0900

    feat : UserDetails 인터페이스의 구현체인 UserDetailsImpl 클래스 구현

---
+++ src/main/java/junhyeok/giveme/global/security/UserDetailsImpl.java
@@ -0,0 +1,58 @@
+package junhyeok.giveme.global.security;
+
+import junhyeok.giveme.user.enums.Role;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Builder
+@AllArgsConstructor @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class UserDetailsImpl implements UserDetails {
+    private Long id;
+    private Role role;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return role.toString();
+            }
+        });
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
