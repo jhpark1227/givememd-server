e64b18eadb8f1afa53ce778e47c8b0348d2dd42c
jhpark1227 <junhyeok0246@naver.com>
2024-04-08 11:40:16 +0900

    feat : ReadmeRepository에 findByUser 메서드 구현

--- src/main/java/junhyeok/giveme/readme/repository/ReadmeRepository.java
+++ src/main/java/junhyeok/giveme/readme/repository/ReadmeRepository.java
@@ -4,8 +4,11 @@ import junhyeok.giveme.readme.entity.Readme;
 import junhyeok.giveme.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReadmeRepository extends JpaRepository<Readme, Long> {
     Optional<Readme> findByNameAndUser(String name, User user);
+
+    List<Readme> findByUser(User user);
 }
