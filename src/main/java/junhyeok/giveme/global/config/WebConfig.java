1faa1d79e58bb68595c8ee5051d9dd5fde4173e8
jhpark1227 <junhyeok0246@naver.com>
2024-06-04 20:08:42 +0900

    feat : 프론트엔드 배포를 위한 CORS allowedOrigins 설정

--- src/main/java/junhyeok/giveme/global/config/WebConfig.java
+++ src/main/java/junhyeok/giveme/global/config/WebConfig.java
@@ -1,16 +1,19 @@
 package junhyeok.giveme.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${cors.origin}")
+    private String origin;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(origin)
                 .allowedMethods("*")
                 .allowedHeaders("content-type", "authorization")
                 .allowCredentials(true)
